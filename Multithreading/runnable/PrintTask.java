32d732a7d2c3e4a6eb582757d216b72cde846e26
marajulcsecu <marajul.cu@gmail.com>
2025-01-11 18:26:28 +0600

    just modified for practice

--- Multithreading/runnable/PrintTask.java
+++ Multithreading/runnable/PrintTask.java
@@ -5,6 +5,7 @@ public class PrintTask implements Runnable {
 
     @Override
     public void run() {
+
         //printTask
         for (int i = 1; i <=1000 ; i++) {
             System.out.printf("%d%c ",i,targetChar);
@@ -12,6 +13,7 @@ public class PrintTask implements Runnable {
         System.out.printf("\n %s %c  task completed",
                 Thread.currentThread().getName(),
                 targetChar);
+
     }
     private final char targetChar;
     public PrintTask(char targetChar) {
